f63470145438df64043c5c17291d256235631cf5
Rohan Mitchell <rohan@rohanmitchell.com>
2011-11-08 07:43:40 +1100

    Add a model and some randomly chosen index views.

--- splitter.rb
+++ splitter.rb
@@ -2,4 +2,24 @@ require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 
+# -- Model
+
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/splitter.db")
+
+class SplitData
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :experiment, String
+  property :path, String
+  property :source, String
+end
+
+SplitData.auto_migrate! unless SplitData.storage_exists?
+
+
+# -- Views
+
+get '/' do
+  haml (rand > 0.5 ? :index_a : :index_b)
+end
